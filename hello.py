7d70e2c26dc152a052d84da36de854753e09de98
sfrnld <aldi@sfrnld.net>
2013-10-24 17:58:08 +0000

    set host & port to run

--- hello.py
+++ hello.py
@@ -7,13 +7,12 @@ conn = Connection('127.0.0.1', 27017)
 @app.route("/hello")
 def find():
 	dbHello = conn['hello']
-	result = []
 	
-	return dbHello['demo'].find()[0]['message']
+	return dbHello['demo'].find()[0]['message'] + '\n'
 	
 @app.route('/')
 def hello():
 	return "Hello World!\n"
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host="0.0.0.0", port=80)
